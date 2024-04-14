1b94c0fa649aed8db1b696bc1940c6adfd624fcd
Abhishek Goyal <agoyal0308@gmail.com>
2024-04-14 15:32:29 +0530

    utils code (ApiError, ApiResponse)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-
-
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
+  });
