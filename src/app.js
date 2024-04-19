f29071d680c8db65bae6cc7738842e3516dd1cc6
Abhishek Goyal <agoyal0308@gmail.com>
2024-04-19 16:25:45 +0530

    user controller

--- src/app.js
+++ src/app.js
@@ -25,5 +25,12 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter);
+
 
 export { app };
